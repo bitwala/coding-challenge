8d4662caf3e605435afa02f8316941026797d523
Constantin Vennekel <constantin.vennekel@nuri.com>
2023-06-16 11:53:48 +0200

    Add GraphQL queries for transaction, block and mempool info
    This commit adds new GraphQL queries to the DataResolver class that allow users to retrieve information about transactions, blocks and mempool. It also includes new ObjectType classes for Transaction, Block, MempoolInfo, TransactionStatus and BlockStatus.

--- src/data/data.resolver.ts
+++ src/data/data.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { DataService } from '../data/data.service';
 import { ObjectType, Field, ID } from '@nestjs/graphql';
 
@@ -8,17 +8,128 @@ export class HashData {
   hash: string;
 }
 
+@ObjectType()
+export class TransactionStatus {
+  @Field()
+  confirmed: boolean;
+
+  @Field({ nullable: true })
+  block_height: number;
+
+  @Field({ nullable: true })
+  block_hash: string;
+}
+@ObjectType()
+export class Transaction {
+  @Field()
+  txid: string;
+
+  @Field()
+  version: number;
+
+  @Field()
+  locktime: number;
+
+  @Field()
+  size: number;
+
+  @Field()
+  weight: number;
+
+  @Field()
+  fee: number;
+
+  // Assuming vin and vout as arrays of strings
+  @Field((type) => [String])
+  vin: string[];
+
+  @Field((type) => [String])
+  vout: string[];
+
+  // Status can be a nested object
+  @Field((type) => TransactionStatus)
+  status: TransactionStatus;
+}
+
+@ObjectType()
+export class BlockStatus {
+  @Field()
+  in_best_chain: boolean;
+
+  @Field({ nullable: true })
+  next_best: string;
+}
+@ObjectType()
+export class Block {
+  @Field()
+  id: string;
+
+  @Field()
+  height: number;
+
+  @Field()
+  version: number;
+
+  @Field()
+  timestamp: number;
+
+  @Field()
+  tx_count: number;
+
+  @Field()
+  size: number;
+
+  @Field()
+  weight: number;
+
+  @Field()
+  merkle_root: string;
+
+  @Field((type) => BlockStatus)
+  status: BlockStatus;
+}
+
+@ObjectType()
+export class MempoolInfo {
+  @Field()
+  count: number;
+
+  @Field()
+  vsize: number;
+
+  @Field()
+  total_fee: number;
+
+  @Field((type) => [[Number]])
+  fee_histogram: number[][];
+}
+
 @Resolver('Data')
 export class DataResolver {
   constructor(private readonly dataService: DataService) {}
 
-  @Query((returns) => String)
-  async getData() {
-    return await this.dataService.getData();
+  @Query(() => String)
+  async getFirstBlockHash() {
+    return await this.dataService.getFirstBlockHash();
   }
 
-  @Query((returns) => String)
+  @Query(() => String)
   async getLatestBlockhash() {
     return await this.dataService.getLatestBlockhash();
+  }
+
+  @Query(() => Transaction)
+  async getTransaction(@Args('txid') txid: string) {
+    return await this.dataService.getTransaction(txid);
+  }
+
+  @Query(() => Block)
+  async getBlock(@Args('hash') hash: string) {
+    return await this.dataService.getBlock(hash);
+  }
+
+  @Query(() => MempoolInfo)
+  async getMempoolInfo() {
+    return await this.dataService.getMempool();
   }
 }

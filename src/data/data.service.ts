9a64f9d56449c68ca7e290d05eece4044f4a9859
Constantin Vennekel <constantin.vennekel@nuri.com>
2023-06-16 11:53:57 +0200

    Refactor DataService to include new transaction, address, block and mempool endpoints
    This commit refactors the DataService class to include new methods for retrieving transaction information such as status, hex, merkle proof and outspends. It also includes new methods for retrieving address information such as transactions and UTXOs. Additionally, it adds methods for retrieving block information including transactions and status. Finally, it includes new methods for accessing mempool data and fee estimates.

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -5,13 +5,108 @@ const blockstreamUrl = 'https://blockstream.info/api';
 
 @Injectable()
 export class DataService {
-  async getData(): Promise<any> {
+  async getFirstBlockHash(): Promise<any> {
     const response = await axios.get(`${blockstreamUrl}/block-height/1`);
     return response.data;
   }
 
   async getLatestBlockhash(): Promise<any> {
     const response = await axios.get(`${blockstreamUrl}/blocks/tip/hash`);
+    return response.data;
+  }
+
+  // Transaction endpoints
+  async getTransaction(txid: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/tx/${txid}`);
+    return response.data;
+  }
+
+  async getTransactionStatus(txid: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/tx/${txid}/status`);
+    return response.data;
+  }
+
+  async getTransactionHex(txid: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/tx/${txid}/hex`);
+    return response.data;
+  }
+
+  async getTransactionMerkleProof(txid: string): Promise<any> {
+    const response = await axios.get(
+      `${blockstreamUrl}/tx/${txid}/merkle-proof`,
+    );
+    return response.data;
+  }
+
+  async getTransactionOutspend(txid: string, vout: number): Promise<any> {
+    const response = await axios.get(
+      `${blockstreamUrl}/tx/${txid}/outspend/${vout}`,
+    );
+    return response.data;
+  }
+
+  async getTransactionOutspends(txid: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/tx/${txid}/outspends`);
+    return response.data;
+  }
+
+  async postTransaction(hex: string): Promise<any> {
+    const response = await axios.post(`${blockstreamUrl}/tx`, hex);
+    return response.data;
+  }
+
+  // Address endpoints
+  async getAddressInfo(address: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/address/${address}`);
+    return response.data;
+  }
+
+  async getAddressTransactions(address: string): Promise<any> {
+    const response = await axios.get(
+      `${blockstreamUrl}/address/${address}/txs`,
+    );
+    return response.data;
+  }
+
+  async getAddressUTXO(address: string): Promise<any> {
+    const response = await axios.get(
+      `${blockstreamUrl}/address/${address}/utxo`,
+    );
+    return response.data;
+  }
+
+  // Block endpoints
+  async getBlock(hash: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/block/${hash}`);
+    return response.data;
+  }
+
+  async getBlockStatus(hash: string): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/block/${hash}/status`);
+    return response.data;
+  }
+
+  async getBlockTransactions(hash: string, startIndex: number): Promise<any> {
+    const response = await axios.get(
+      `${blockstreamUrl}/block/${hash}/txs/${startIndex}`,
+    );
+    return response.data;
+  }
+
+  // Mempool endpoints
+  async getMempool(): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/mempool`);
+    return response.data;
+  }
+
+  async getMempoolTxids(): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/mempool/txids`);
+    return response.data;
+  }
+
+  // Fee estimate endpoint
+  async getFeeEstimates(): Promise<any> {
+    const response = await axios.get(`${blockstreamUrl}/fee-estimates`);
     return response.data;
   }
 }
